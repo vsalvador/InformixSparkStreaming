832e4db66ebe0cd5fc576baca777a8c79d694a36
informix <informix@localhost.localdomain>
2026-03-27 11:53:26 +0100

    Compile library with debug messages

--- setup.sh
+++ setup.sh
@@ -45,6 +45,7 @@ fi
 
 MAIN_DIR=`pwd`
 LIB_DIR=$MAIN_DIR/lib
+LIB_DIR=$INFORMIXDIR
 
 echo "Current dir: $MAIN_DIR"
 echo "Lib dir: $LIB_DIR"
@@ -164,7 +165,7 @@ echo -e "--------------------------------------";
 echo -e "Building UDR..."
 echo -e "--------------------------------------\n";
 
-gcc -Wall -fPIC -DMI_SERVBUILD -I$LIB_DIR/include -I$INFORMIXDIR/incl/public -I$INFORMIXDIR/incl -c *.c
+gcc -Wall -fPIC -DWITH_ISS_DEBUG -DMI_SERVBUILD -I$LIB_DIR/include -I$INFORMIXDIR/incl/public -I$INFORMIXDIR/incl -c *.c
 if_cmd_failed
 gcc -Wl,-rpath=$LIB_DIR/lib,-L$LIB_DIR/lib -lwolfssl -lwolfmqtt *.o -shared -o informix_socket_streaming.so
 if_cmd_failed
